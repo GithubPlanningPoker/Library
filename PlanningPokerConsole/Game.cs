b42d528254730b4271bd848d3f0a1efd05973f3f
deaddog <mikkelslsl@gmail.com>
2014-10-29 10:22:39 +0100

    A game can be joined through a static method.

--- PlanningPokerConsole/Game.cs
+++ PlanningPokerConsole/Game.cs
@@ -9,6 +9,7 @@ namespace PlanningPokerConsole
 {
     public class Game
     {
+        private bool host;
         private readonly Id id;
         private readonly User user;
 
@@ -19,11 +20,21 @@ namespace PlanningPokerConsole
             Id gameid = new Id(json["gameid"].Value<string>());
             User user = new User(username, new Id(json["userid"].Value<string>()));
 
-            return new Game(gameid, user);
+            return new Game(true, gameid, user);
         }
 
-        private Game(Id id, User user)
+        public static Game JoinGame(Id gameid, string username)
         {
+            var json = JsonRequestHandler.Request("/game/" + gameid.Hash + "/user/", RequestMethods.POST, "{ \"name\" : \"" + username + "\" }");
+            User user = new User(username, new Id(json["userid"].Value<string>()));
+
+            return new Game(false, gameid, user);
+        }
+
+        private Game(bool host, Id id, User user)
+        {
+            this.host = host;
+
             if (id == null)
                 throw new ArgumentNullException("id");
             this.id = id;
@@ -31,6 +42,21 @@ namespace PlanningPokerConsole
             if (user == null)
                 throw new ArgumentNullException("user");
             this.user = user;
+        }
+
+        public bool Host
+        {
+            get { return host; }
+        }
+
+        public Id Id
+        {
+            get { return id; }
+        }
+
+        public User User
+        {
+            get { return user; }
         }
     }
 }
