2eba4e1d3c8d1437a2fa84828d7cd259381ccc78
deaddog <mikkelslsl@gmail.com>
2014-10-29 10:21:42 +0100

    A Game contains a User element, which is set through the constructor.

--- PlanningPokerConsole/Game.cs
+++ PlanningPokerConsole/Game.cs
@@ -10,23 +10,27 @@ namespace PlanningPokerConsole
     public class Game
     {
         private readonly Id id;
+        private readonly User user;
 
-        public static Game CreateGame(string username, out Id userID)
+        public static Game CreateGame(string username)
         {
             var json = JsonRequestHandler.Request("/game/", RequestMethods.POST, "{ \"name\" : \"" + username + "\" }");
 
-            Id gameid = new Id((json["gameid"] as JValue).Value<string>());
-            userID = new Id((json["userid"] as JValue).Value<string>());
+            Id gameid = new Id(json["gameid"].Value<string>());
+            User user = new User(username, new Id(json["userid"].Value<string>()));
 
-            return new Game(gameid);
+            return new Game(gameid, user);
         }
 
-        public Game(Id id)
+        private Game(Id id, User user)
         {
             if (id == null)
                 throw new ArgumentNullException("id");
-
             this.id = id;
+
+            if (user == null)
+                throw new ArgumentNullException("user");
+            this.user = user;
         }
     }
 }
