f4e3b9a761a3c4ac767503ff5f0d8e4326614f61
deaddog <mikkelslsl@gmail.com>
2014-10-29 10:18:49 +0100

    Created a User class.

---
+++ PlanningPokerConsole/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanningPokerConsole
+{
+    public class User
+    {
+        private readonly Id id;
+        private string name;
+
+        public User(string name, Id id)
+            : this(name)
+        {
+            if (id == null)
+                throw new ArgumentNullException("id");
+
+            this.id = id;
+        }
+
+        public User(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+            
+            this.name = name;
+            this.id = null;
+        }
+
+        public Id Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
