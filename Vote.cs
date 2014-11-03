448556700b194a0b16ac8fb52028937bdb0e0b7f
deaddog <mikkelslsl@gmail.com>
2014-11-03 16:59:55 +0100

    Created a vote class that will represent the votes returned from the api.

---
+++ Vote.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Vote
+    {
+        private string name;
+        private VoteTypes? votetype;
+
+        public Vote(string name, VoteTypes? votetype)
+        {
+            this.name = name;
+            this.votetype = votetype;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        public VoteTypes? VoteType
+        {
+            get { return votetype; }
+        }
+    }
+}
