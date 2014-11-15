f1f20316743e8f4b57e5c2d79acf8ad8ecbe494a
Bruno Thalmann <mr.thalmann@gmail.com>
2014-11-15 18:28:58 +0100

    userid changed to userId

--- Game.cs
+++ Game.cs
@@ -33,7 +33,7 @@ namespace Library
 
             string request = string.Format("/game/{0}/user/", gameid.Hash);
             var json = handler.Request(request, RequestMethods.POST, new JObject(new JProperty("username", username)));
-            User user = new User(username, new Id(json["userid"].Value<string>()));
+            User user = new User(username, new Id(json["userId"].Value<string>()));
 
             return new Game(false, gameid, user, handler);
         }
